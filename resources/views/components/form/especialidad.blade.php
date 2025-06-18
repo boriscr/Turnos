c632adeb0001a28b292a569e3ca06fee24e1d1be
Boris <boriscruzr@gmail.com>
2025-06-18 12:26:40 -0300

    Unificacion del formulario para crear nuevas especialidades desde el formulario de equipos

--- resources/views/components/form/especialidad.blade.php
+++ resources/views/components/form/especialidad.blade.php
@@ -5,14 +5,14 @@
     @endif
     <div class="item">
         <label for="nombre"><span aria-hidden="true" style="color: red;">*</span> Nombre de la especialidad</label>
-        <input type="text" name="nombre" id="nombre" required value="{{ $nombre }}">
+        <input type="text" name="nombre" id="nombre" placeholder="Ej. Nutricionista, Traumatólogo, Odontología..." required value="{{ $nombre }}">
         @error('nombre')
             <div class="error">{{ $message }}</div>
         @enderror
     </div>
     <div class="item">
-        <label for="descripcion">Descripcion</label>
-        <textarea name="descripcion" id="descripcion" cols="30" rows="5">{{ $descripcion }}</textarea>
+        <label for="descripcion"><span aria-hidden="true" style="color: red;">*</span>Descripcion</label>
+        <textarea name="descripcion" id="descripcion" cols="30" rows="5" placeholder="Describe las funciones y servicios que ofrece esta especialidad. Puedes incluir ubicaciones, requisitos o advertencias para los pacientes." required>{{ $descripcion }}</textarea>
         @error('descripcion')
             <div class="error">{{ $message }}</div>
         @enderror
@@ -20,7 +20,7 @@
 
     <div class="item">
         <label for="descripcion-esp"><span aria-hidden="true" style="color: red;">*</span> Estado</label>
-        <select name="estado" id="estado">
+        <select name="estado" id="estado" required>
             <option value="1"{{ isset($edit) ? '' : 'selected' }}
                 {{ isset($estado) && $estado == 1 ? 'selected' : '' }}>Activo</option>
             <option value="0" {{ isset($estado) && $estado == 0 ? 'selected' : '' }}>Inactivo</option>
--- resources/views/equipos/create.blade.php
+++ resources/views/equipos/create.blade.php
@@ -2,60 +2,16 @@
     <div class="main">
         <div class="container-form">
             <h3 class="title-form">Agregar nuevo equipo</h3>
-            <x-form.equipo :ruta="route('equipo.store')" 
-            :crear="true" 
-            :especialidades="$especialidades" 
-            :nombre="old('nombre')" 
-            :apellido="old('apellido')"
-            :dni="old('dni')" 
-            :matricula="old('matricula')" 
-            :email="old('email')" 
-            :telefono="old('telefono')" 
-            :role="old('role')"
-            :estado="old('estado')" 
-            :especialidad="old('especialidad')" 
-            />
-            
+            <x-form.equipo :ruta="route('equipo.store')" :crear="true" :especialidades="$especialidades" :nombre="old('nombre')" :apellido="old('apellido')"
+                :dni="old('dni')" :matricula="old('matricula')" :email="old('email')" :telefono="old('telefono')" :role="old('role')"
+                :estado="old('estado')" :especialidad="old('especialidad')" />
+
+            {{-- Formulario para crear una nueva especialidad --}}
             <div class="form-especialidad" id="especialidad-form">
-                <form action="{{ route('especialidad.store') }}" method="post">
-                    @csrf
-                    <h3 class="title-form">Crear una nueva especialidad</h3>
-                    <div class="item">
-                        <label for="nombre"><span aria-hidden="true" style="color: red;">*</span>Nombre de la
-                            especialidad</label>
-                        <input type="text" name="nombre" id="nombre" required value="{{ old('nombre') }}">
-                    </div>
-                    <div class="item">
-                        <label for="descripcion">Descripcion</label>
-                        <textarea name="descripcion" id="descripcion" cols="30" rows="5">{{ old('descripcion') }}</textarea>
-                    </div>
-
-                    <div class="item switch-item">
-                        <label for="descripcion-esp"><b>Estado</b></label>
-                        <div class="switch">
-                            <input type="checkbox" id="toggle-esp" name="estado" value="1" checked>
-                            <label for="toggle-esp"></label>
-                        </div>
-                        <p id="estadoTxt-esp" class="estadoTxt">Activo</p>
-                    </div>
-                    <button type="submit" class="submit-btn">Registrar</button>
-                    <button type="button" id="close-btn" style="background: red; color: #fff;">Cancelar</button>
-                </form>
-
-                @if ($errors->any())
-                    <div style="color: red;">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
-
-
-                <script src="../forms/switch.js"></script>
-                <script src="../forms/especialidad.js"></script>
-
+                <x-form.especialidad :ruta="route('especialidad.store')" 
+                :crear="true" :nombre="old('nombre')" 
+                :descripcion="old('descripcion')" />
+                <script src="../../forms/especialidad.js"></script>
             </div>
         </div>
 </x-body.body>
