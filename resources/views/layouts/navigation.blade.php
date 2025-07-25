57334abb12333960ea517cd883c4f6344f7cfdaa
Boris <boriscruzr@gmail.com>
2025-07-25 16:34:41 -0300

    Traduccion de elementos con helpers, creacion de nuevo componente para el navbar, se agregó iconos, correccion de errores en pantallas mas pequeñas

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,7 +1,7 @@
 <nav x-data="{ open: false }" class="main-nav border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="navbar h-16">
+        <div class="navbar navbar-box h-16 sm:flex">
             <!-- Logo -->
             <div class="shrink-0 flex items-center text-xl font-bold">
                 <a href="{{ route('home') }}">
@@ -11,12 +11,13 @@
             <div class="navbar-items hidden sm:flex justify-between">
                 <!-- Navigation Links -->
                 <!-- Menú principal -->
-                <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
-                    {{ __('home') }}
-                </x-nav-link>
-                <x-nav-link :href="route('disponible.create')" :active="request()->routeIs('disponible.create')">
-                    {{ __('Reservar Turno') }}
-                </x-nav-link>
+                <x-nav-link-active href="{{ route('home') }}" route="home">
+                    <i class="bi bi-house"></i><span>{{ __('home') }}</span>
+                </x-nav-link-active>
+
+                <x-nav-link-active href="{{ route('disponible.create') }}" route="disponible.create">
+                    <i class="bi bi-calendar-plus"></i><span>{{ __('Book Appointment') }}</span>{{-- Reservar Turno --}}
+                </x-nav-link-active>
 
                 <!-- Settings Dropdown -->
                 @role('admin')
@@ -25,7 +26,8 @@
                             <x-slot name="trigger">
                                 <button
                                     class="menu-btn inline-flex items-center leading-4 font-medium rounded-md focus:outline-none transition ease-in-out duration-150">
-                                    <div>Administración
+                                    <div><i class="bi bi-clipboard-pulse"></i>
+                                        <span>{{ __('Administration') }}</span>{{-- Administracion --}}
                                     </div>
 
                                     <div class="ms-1">
@@ -41,49 +43,59 @@
 
                             <x-slot name="content">
                                 <div class="dropdown-section">
-                                    <div class="subtitulo">Sección Usuarios</div>
+
+                                    <!-- Sección Usuarios -->
+                                    <div class="subtitulo">{{ __('Manage Users') }}</div>{{-- Gestionar Usuarios --}}
                                     <x-dropdown-link :href="route('usuario.index')">
-                                        <i class="bi bi-eye"></i> Ver usuarios
-                                    </x-dropdown-link>
-                                    <div class="subtitulo">Sección Medicos</div>
-                                    <x-dropdown-link :href="route('medico.index')">
-                                        <i class="bi bi-eye"></i> Ver medicos
-                                    </x-dropdown-link>
-                                    <x-dropdown-link :href="route('medico.create')">
-                                        <i class="bi bi-person-fill-add"></i> Agregar medicos
+                                        <i class="bi bi-eye"></i> <span> {{ __('View Users') }}</span>
+                                        {{-- Ver usuarios --}}
                                     </x-dropdown-link>
 
-                                    <x-dropdown-link :href="route('especialidad.index')">
-                                        <i class="bi bi-heart-pulse-fill"></i> Especialidades
+                                    <!-- Sección Medicos -->
+                                    <div class="subtitulo">{{ __('Manage Doctors') }}</div>{{-- Gestionar Medicos --}}
+                                    <x-dropdown-link :href="route('medico.index')">
+                                        <i class="bi bi-eye"></i><span> {{ __('View Doctors') }}</span>
+                                        {{-- Ver medicos --}}
                                     </x-dropdown-link>
-                                    <div class="subtitulo">Sección Turnos</div>
+                                    <x-dropdown-link :href="route('medico.create')">
+                                        <i class="bi bi-person-fill-add"></i> <span>
+                                            {{ __('Add Doctor') }}</span>{{-- Agregar medicos --}}
+                                    </x-dropdown-link>
+                                    <x-dropdown-link :href="route('especialidad.index')">
+                                        <i class="bi bi-heart-pulse-fill"></i> <span>
+                                            {{ __('Specialties') }}</span>{{-- Especialidades --}}
+                                    </x-dropdown-link>
+
+                                    <!-- Sección Turnos -->
+                                    <div class="subtitulo"> {{ __('Manage Appointments') }}</div>{{-- Gestionar Turnos --}}
                                     <x-dropdown-link :href="route('turnos.index')">
-                                        <i class="bi bi-eye"></i> Ver Turnos
+                                        <i class="bi bi-eye"></i> <span>
+                                            {{ __('View Appointments') }}</span>{{-- Ver Turnos --}}
                                     </x-dropdown-link>
                                     <x-dropdown-link :href="route('turnos.create')">
-                                        <i class="bi bi-clock"></i> Crear Turnos
+                                        <i class="bi bi-clock"></i> <span>
+                                            {{ __('Create Appointment') }}</span>{{-- Crear Turnos --}}
                                     </x-dropdown-link>
-                                    <div class="subtitulo">Sección Reservas</div>
+
+                                    <!-- Sección Reservas -->
+                                    <div class="subtitulo"> {{ __('Manage Booking') }}</div>{{-- Gestionar Reservas --}}
                                     <x-dropdown-link :href="route('reservas.index')">
-                                        <i class="bi bi-eye"></i> Ver reservas
+                                        <i class="bi bi-eye"></i> <span>
+                                            {{ __('View Booking') }}</span>{{-- Ver reservas --}}
                                     </x-dropdown-link>
                                 </div>
                             </x-slot>
                         </x-dropdown>
                     </div>
-                    <x-nav-link :href="route('settings.edit')" :active="request()->routeIs('settings.edit')">
-                        {{ __('Settings') }}
-                    </x-nav-link>
+
+                    <!-- Settings Dropdown -->
+                    <x-nav-link-active href="{{ route('settings.edit') }}" route="settings.edit">
+                        <i class="bi bi-gear-fill"></i><span>{{ __('Settings') }}</span>
+                    </x-nav-link-active>
                 @endrole
-
-
-                <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                    {{ __('Panel') }}
-                </x-nav-link>
             </div>
-            <!-- Settings Dropdown -->
             @if (auth()->check())
-                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                <div class="hidden sm:flex justify-between">
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             <button
@@ -104,9 +116,11 @@
 
                         <x-slot name="content">
                             <x-dropdown-link :href="route('profile.index')">
-                                {{ __('Perfil') }}
+                                <i class="bi bi-person-circle"></i><span>{{ __('Profile') }}</span>
                             </x-dropdown-link>
-
+                            <x-dropdown-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                                <i class="bi bi-speedometer2"></i><span>{{ __('Dashboard') }}</span>
+                            </x-dropdown-link>
                             <!-- Authentication -->
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
@@ -114,7 +128,7 @@
                                 <x-dropdown-link class="btn-salir" :href="route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                    {{ __('Salir') }}
+                                    <i class="bi bi-box-arrow-left"></i><span>{{ __('Logout') }}</span>
                                 </x-dropdown-link>
                             </form>
                         </x-slot>
@@ -122,12 +136,12 @@
                 </div>
             @else
                 <div class="hidden sm:flex sm:items-center sm:ms-6">
-                    <x-nav-link :href="route('login')">
-                        {{ __('Iniciar sesión') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('register')">
-                        {{ __('Registrarse') }}
-                    </x-nav-link>
+                    <x-nav-link-active :href="route('login')" route="login">
+                        {{ __('Login') }}
+                    </x-nav-link-active>
+                    <x-nav-link-active :href="route('register')" route="register">
+                        {{ __('Register') }}
+                    </x-nav-link-active>
                 </div>
             @endif
             <!-- Hamburger -->
@@ -154,17 +168,21 @@
     <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('home')" :active="request()->routeIs('home')">
-                {{ __('Home') }}
+                <i class="bi bi-house"></i><span>{{ __('home') }}</span>
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('disponible.create')" :active="request()->routeIs('disponible.create')">
-                {{ __('Reservar Turno') }}
+                <i class="bi bi-calendar-plus"></i><span>{{ __('Book Appointment') }}</span>{{-- Reservar Turno --}}
             </x-responsive-nav-link>
 
             @role('admin')
                 <div x-data="{ adminOpen: false }" class="space-y-1">
                     <button @click="adminOpen = !adminOpen" class="w-full text-left px-4 py-2">
                         <div class="flex items-center justify-between">
-                            <span>Administración</span>
+                            <div>
+                                <i class="bi bi-clipboard-pulse"></i>
+                                <span>{{ __('Administration') }}</span>{{-- Administracion --}}
+
+                            </div>
                             <svg class="h-4 w-4 transform transition-transform" :class="{ 'rotate-180': adminOpen }"
                                 fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
@@ -176,48 +194,52 @@
                         <!-- Versión móvil del dropdown -->
                         <div class="menu pt-2 pb-2 space-y-1">
                             <!-- Sección Usuarios -->
-                            <div class="subtitulo">Sección Usuarios</div>
+                            <div class="subtitulo">{{ __('Manage Users') }}</div>{{-- Gestionar Usuarios --}}
                             <x-responsive-nav-link :href="route('usuario.index')">
-                                <i class="bi bi-eye"></i> Ver usuarios
+                                <i class="bi bi-eye"></i> <span> {{ __('View Users') }}</span>
+                                {{-- Ver usuarios --}}
                             </x-responsive-nav-link>
 
                             <!-- Sección Medico -->
-                            <div class="subtitulo">Sección Medicos</div>
+                            <div class="subtitulo">{{ __('Manage Doctors') }}</div>{{-- Gestionar Medicos --}}
                             <x-responsive-nav-link :href="route('medico.index')">
-                                <i class="bi bi-eye"></i> Ver medicos
+                                <i class="bi bi-eye"></i><span> {{ __('View Doctors') }}</span>
+                                {{-- Ver medicos --}}
                             </x-responsive-nav-link>
                             <x-responsive-nav-link :href="route('medico.create')">
-                                <i class="bi bi-person-fill-add"></i> Agregar medicos
+                                <i class="bi bi-person-fill-add"></i> <span>
+                                    {{ __('Add Doctor') }}</span>{{-- Agregar medicos --}}
                             </x-responsive-nav-link>
 
                             <x-responsive-nav-link :href="route('especialidad.index')">
-                                <i class="bi bi-heart-pulse-fill"></i> Especialidades
+                                <i class="bi bi-heart-pulse-fill"></i> <span>
+                                    {{ __('Specialties') }}</span>{{-- Especialidades --}}
                             </x-responsive-nav-link>
 
                             <!-- Sección Turnos -->
-                            <div class="subtitulo">Sección Turnos</div>
+                            <div class="subtitulo"> {{ __('Manage Appointments') }}</div>{{-- Gestionar Turnos --}}
                             <x-responsive-nav-link :href="route('turnos.index')">
-                                <i class="bi bi-eye"></i> Ver Turnos
+                                <i class="bi bi-eye"></i> <span>
+                                    {{ __('View Appointments') }}</span>{{-- Ver Turnos --}}
                             </x-responsive-nav-link>
                             <x-responsive-nav-link :href="route('turnos.create')">
-                                <i class="bi bi-clock"></i> Crear Turnos
+                                <i class="bi bi-clock"></i> <span>
+                                    {{ __('Create Appointment') }}</span>{{-- Crear Turnos --}}
                             </x-responsive-nav-link>
 
                             <!-- Sección Reservas -->
-                            <div class="subtitulo">Sección Reservas</div>
+                            <div class="subtitulo"> {{ __('Manage Booking') }}</div>{{-- Gestionar Reservas --}}
                             <x-responsive-nav-link :href="route('reservas.index')">
-                                <i class="bi bi-eye"></i> Ver reservas
+                                <i class="bi bi-eye"></i> <span>
+                                    {{ __('View Booking') }}</span>{{-- Ver reservas --}}
                             </x-responsive-nav-link>
                         </div>
 
                     </div>
                     <x-responsive-nav-link :href="route('settings.edit')">
-                        {{ __('Settings') }}
+                        <i class="bi bi-gear-fill"></i><span>{{ __('Settings') }}</span>
                     </x-responsive-nav-link>
                 @endrole
-                <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                    {{ __('Panel') }}
-                </x-responsive-nav-link>
 
                 <!-- Responsive Settings Options -->
                 @if (auth()->check())
@@ -229,10 +251,12 @@
                         </div>
 
                         <div class="mt-3 space-y-1">
-                            <x-responsive-nav-link :href="route('profile.edit')">
-                                {{ __('Perfil') }}
+                            <x-responsive-nav-link :href="route('profile.index')" :active="request()->routeIs('profile.index')">
+                                <i class="bi bi-person-circle"></i><span>{{ __('Profile') }}</span>
                             </x-responsive-nav-link>
-
+                            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                                <i class="bi bi-speedometer2"></i><span>{{ __('Dashboard') }}</span>
+                            </x-responsive-nav-link>
                             <!-- Authentication -->
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
@@ -240,23 +264,23 @@
                                 <x-responsive-nav-link class="btn-salir" :href="route('logout')"
                                     onclick="event.preventDefault();
                                         this.closest('form').submit();">
-                                    {{ __('Salir') }}
+                                    <i class="bi bi-box-arrow-left"></i><span>{{ __('Logout') }}</span>
                                 </x-responsive-nav-link>
                             </form>
                         </div>
                     </div>
                 @else
                     <div class="px-4">
-                        <div class="font-medium text-base text-gray-800 dark:text-gray-200">Inicie sesion o cree una
+                        <div class="font-medium text-base">Inicie sesion o cree una
                             cuenta.
                         </div>
                     </div>
                     <div class="mt-3 space-y-1">
                         <x-responsive-nav-link :href="route('login')">
-                            {{ __('Iniciar sesión') }}
+                            {{ __('Login') }}
                         </x-responsive-nav-link>
                         <x-responsive-nav-link :href="route('register')">
-                            {{ __('Registrarse') }}
+                            {{ __('Register') }}
                         </x-responsive-nav-link>
                 @endif
             </div>
